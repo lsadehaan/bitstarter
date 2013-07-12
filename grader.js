dc1039767bf322c79a9450f08629cc95c863bbd6
Adriaan de Haan <adriaan.dehaan@gmail.com>
2013-07-12 03:19:07 +0000

    fixed mistake in index.html

--- grader.js
+++ grader.js
@@ -20,10 +20,12 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var sys = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var URL_DEFAULT = "http://evening-plateau-4069.herokuapp.com/";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,6 +33,15 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var assertValidURL = function(inURL) {
+    var instr = inURL.toString();
+    if(instr.split('.').length < 2) {
+        console.log("%s is not a valid URL. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
@@ -55,6 +66,38 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlURL = function(URL, checksfile) {
+    var retries = 0;
+    var cheeryResult;
+    var callback = function(result){
+	if (retries != 0){
+	    retries = 0;
+	}
+	if (result instanceof Error){
+	    console.log("Error loading URL: " + result.message);
+	    if (retries < 3){
+		retries = retries + 1;
+		console.log(retries + " of 3 retries.");
+		retry(3000); // try again after 3 sec.
+	    }
+	} else {
+	    cheeryResult = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = cheeryResult(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.log(out);
+	}
+    };
+
+    var result = rest.get(URL).on('complete', callback);
+
+    return;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -63,12 +106,21 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'URL to html', clone(assertValidURL), URL_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.file){
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    } else {
+	var checkJson = checkHtmlURL(program.url, program.checks);
+	JSON.stringify(checkJson, null, 4);
+//	console.log(outJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlURL = checkHtmlURL;
 }
